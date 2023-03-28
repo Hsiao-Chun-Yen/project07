86cce044385461fbaffee968674545a1cc43ec1e
hsiao <vickyu96431@gmail.com>
2023-03-28 20:37:58 +0800

    修改AOS無法正常出現的問題

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   setTimeout(function() {
      document.querySelector('main').style.display = "block";
      document.querySelector('header').style.display = "none";
+     AOS.refresh();
   }, 2000);
 })
 
